dcd0d79a846a67990c43499f8281d4d0f02e56dd
Abril-star-12 <abrilcastro5238@gmail.com>
2024-09-26 23:29:57 -0300

    nav

---
+++ DESAFIOS/assets/main.js
@@ -0,0 +1,18 @@
+
+//alert("hola desde main.js")
+console.error ("error");
+
+let X = 10;
+const Y = 5;
+
+let nombre = "ramiro";
+let edad = 31;
+
+X = "alexis";
+
+console.log ("tu nombre es:" + X + "y tu edad es:" + edad);
+console.log (`tu nombre es:${nombre} y tu edad es: ${edad}`);
+
+const NU = prompt (`Ingre su nombre:`)
+
+console.log(NU)
